c2dca72ca546976836c1794ddd2405b0346310dd
Audrey Sobgou Zebaze <audrey.sobgouzebaze@gmail.com>
2020-10-31 12:23:21 +0100

    [QRCode] fix qrCode generation

--- Attestation/Attestation+QRCode.swift
+++ Attestation/Attestation+QRCode.swift
@@ -6,17 +6,18 @@
 //
 
 import UIKit
+import CoreImage.CIFilterBuiltins
 
 extension Attestation {
     private func generateQRCode(from string: String) -> UIImage? {
-        let data = string.data(using: String.Encoding.ascii)
-
-        if let filter = CIFilter(name: "CIQRCodeGenerator") {
-            filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
-
-            if let output = filter.outputImage?.transformed(by: transform) {
-                return UIImage(ciImage: output)
+        let context = CIContext()
+        let filter = CIFilter.qrCodeGenerator()
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage =  filter.outputImage {
+            if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImg)
             }
         }
 
